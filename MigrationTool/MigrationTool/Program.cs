6c08bd5a7bf34e27e04055c636af412843a07cc0
Martyn Price <martyn@devoria.co.uk>
2023-09-04 09:34:09 +0100

    Add creation of context and different data types

--- MigrationTool/MigrationTool/Program.cs
+++ MigrationTool/MigrationTool/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using System.Data;
+using System.Text;
 
 namespace MigrationTool
 {
@@ -10,6 +11,8 @@ namespace MigrationTool
             string connectionString = "Data Source={databaseServer};Initial Catalog={databaseName};Integrated Security=true;";
             string namespaceName = "";
             string outputDirectory = "";
+            string contextNamespace = "";
+            string contextName = "";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -24,6 +27,18 @@ namespace MigrationTool
 
                 // Group the columns by table name
                 Dictionary<string, List<DataRow>> columnsByTable = dataset.Tables[1].AsEnumerable().GroupBy(r => r.Field<string>("TABLE_NAME")).ToDictionary(g => g.Key, g => g.ToList());
+
+                StringBuilder dbContextCode = new StringBuilder();
+
+                dbContextCode.AppendLine($"using Microsoft.EntityFrameworkCore;");
+                dbContextCode.AppendLine($"using {namespaceName};"); // Assuming your entities are in this namespace
+                dbContextCode.AppendLine();
+                dbContextCode.AppendLine($"namespace {contextNamespace}");
+                dbContextCode.AppendLine("{");
+                dbContextCode.AppendLine($"    public class {contextName} : DbContext");
+                dbContextCode.AppendLine("    {");
+                dbContextCode.AppendLine($"        public {contextName}(DbContextOptions<{contextName}> options) : base(options) {{ }}");
+                dbContextCode.AppendLine();
 
                 // Generate the C# code for each table
                 foreach (DataRow tableRow in dataset.Tables[0].Rows)
@@ -52,31 +67,88 @@ namespace MigrationTool
                     // Write the generated C# code to a file
                     string filename = outputDirectory + tableName + ".cs";
                     File.WriteAllText(filename, modelCode);
+
+                    dbContextCode.AppendLine($"        public DbSet<{tableName}> {tableName} {{ get; set; }}");
                 }
+                dbContextCode.AppendLine("    }");
+                dbContextCode.AppendLine("}");
+
+                // Write the DbContext to a file
+                string dbContextFilename = Path.Combine(outputDirectory, $"{contextName}.cs");
+                File.WriteAllText(dbContextFilename, dbContextCode.ToString());
             }
 
             static string GetCSharpType(string dataType, int? maxLength, bool isNullable)
             {
-                if (dataType == "int")
+                switch (dataType)
                 {
-                    return isNullable ? "int?" : "int";
-                }
-                else if (dataType == "varchar" || dataType == "nvarchar" || dataType == "text" || dataType == "ntext")
-                {
-                    return "string";
-                }
-                else if (dataType == "datetime" || dataType == "smalldatetime")
-                {
-                    return isNullable ? "DateTime?" : "DateTime";
-                }
-                else if (dataType == "varbinary")
-                {
-                    return "byte[]";
-                }
-                // Add other data types here as needed
-                else
-                {
-                    throw new Exception($"Unknown data type '{dataType}'");
+                    case "tinyint":
+                        return isNullable ? "byte?" : "byte";
+
+                    case "smallint":
+                        return isNullable ? "short?" : "short";
+
+                    case "int":
+                        return isNullable ? "int?" : "int";
+
+                    case "bigint":
+                        return isNullable ? "long?" : "long";
+
+                    case "bit":
+                        return isNullable ? "bool?" : "bool";
+
+                    case "varchar":
+                    case "nvarchar":
+                    case "text":
+                    case "ntext":
+                    case "char":
+                    case "nchar":
+                        return "string";
+
+                    case "datetime":
+                    case "smalldatetime":
+                    case "datetime2":
+                    case "date":
+                    case "time":
+                        return isNullable ? "DateTime?" : "DateTime";
+
+                    case "timestamp":
+                    case "rowversion":
+                    case "binary":
+                    case "varbinary":
+                    case "image":
+                        return "byte[]";
+
+                    case "uniqueidentifier":
+                        return isNullable ? "Guid?" : "Guid";
+
+                    case "float":
+                        return isNullable ? "double?" : "double";
+
+                    case "real":
+                        return isNullable ? "float?" : "float";
+
+                    case "money":
+                    case "smallmoney":
+                    case "decimal":
+                    case "numeric":
+                        return isNullable ? "decimal?" : "decimal";
+
+                    case "xml":
+                        return "string";  // or System.Xml.Linq.XElement based on use case
+
+                    case "sql_variant":
+                        return "object";
+
+                    case "geometry":
+                    case "geography":
+                        return "Microsoft.SqlServer.Types.SqlGeometry"; // or SqlGeography based on data type
+
+                    case "hierarchyid":
+                        return "Microsoft.SqlServer.Types.SqlHierarchyId";
+
+                    default:
+                        throw new Exception($"Unknown data type '{dataType}'");
                 }
             }
     }
